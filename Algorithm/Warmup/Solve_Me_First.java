21018492f01a99e0d267bb1a6454b40371c5031f
bobypanchal <64546165+bobypanchal@users.noreply.github.com>
2020-04-30 21:34:57 +0530

    Update and rename Algorithm/Warmup/Solve Me First/Solution.java to Algorithm/Warmup/Solve_Me_First.java

--- Algorithm/Warmup/Solve_Me_First.java
+++ Algorithm/Warmup/Solve_Me_First.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Solve_Me_First {
 
     static int solveMeFirst(int a, int b) {
       	return a+b;
