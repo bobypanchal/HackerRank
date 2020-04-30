6bba79fc345279b277cf6d4b6a64e663eddc01ac
bobypanchal <64546165+bobypanchal@users.noreply.github.com>
2020-04-30 21:38:26 +0530

    Update and rename Data Structures/Arrays/Left Rotation/Solution.java to Data Structures/Arrays/Left_Rotation.java

--- Data Structures/Arrays/Left_Rotation.java
+++ Data Structures/Arrays/Left_Rotation.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Left_Rotation {
 
 private static final Scanner scanner = new Scanner(System.in);
 
