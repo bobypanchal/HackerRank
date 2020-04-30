b00e70318e01d283a77ba044797d0099f0025d18
bobypanchal <64546165+bobypanchal@users.noreply.github.com>
2020-04-30 21:39:33 +0530

    Update and rename Data Structures/Arrays/Dynamic Array/Solution.java to Data Structures/Arrays/Dynamic_Array.java

--- Data Structures/Arrays/Dynamic_Array.java
+++ Data Structures/Arrays/Dynamic_Array.java
@@ -43,7 +43,7 @@ class Result {
 
 }
 
-public class Solution {
+public class Dynamic_Array {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
