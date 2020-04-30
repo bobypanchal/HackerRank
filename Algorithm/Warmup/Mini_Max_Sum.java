98e56a76d8f50f0477fd258d56b2670d66c9d619
bobypanchal <64546165+bobypanchal@users.noreply.github.com>
2020-04-30 22:03:23 +0530

    Create Mini_Max_Sum.java

---
+++ Algorithm/Warmup/Mini_Max_Sum.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Mini_Max_Sum {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        Arrays.sort(arr);
+        long min = 0, max = 0;
+        for(int i=0; i<4; i++)
+            min += arr[i];
+        for(int i=arr.length; i>=arr.length-3; i--)
+            max += arr[i-1];
+        System.out.println(min + " " + max);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
