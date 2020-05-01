280ee1f7891bd223b3f9dd980aa3434dd9d9f9b8
Boby Panchal <64546165+bobypanchal@users.noreply.github.com>
2020-05-01 19:49:26 +0530

    Create Jumping_on_the_Clouds_Revisited.java

---
+++ Algorithm/Implementation/Jumping_on_the_Clouds_Revisited.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Jumping_on_the_Clouds_Revisited {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+        int energy = 100;
+        int cloud = 0;
+        do{
+            energy--;
+            cloud = (cloud + k) % c.length;
+            if(c[cloud] == 1)
+                energy -= 2;
+        } while(cloud != 0);
+        return energy;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
